56e018fcd32484c83b9699cbd7cdbcd8496b3f58
gvangalen <gvangalen@hotmail.com>
2025-04-23 22:12:41 +0200

    Update NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,1 +1,8 @@
-
+export default function NavBar() {
+  return (
+    <nav className="bg-black text-white p-3 text-center space-x-6">
+      <a href="/dashboard" className="font-bold">📊 Dashboard</a>
+      <a href="/rapport" className="font-bold">📄 Dagrapport</a>
+    </nav>
+  );
+}

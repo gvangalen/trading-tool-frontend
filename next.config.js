63aa69ccdfdd02153ec53ae75df46922076785c5
gvangalen <gvangalen@hotmail.com>
2025-05-11 20:43:48 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   async redirects() {
     return [
@@ -8,6 +10,18 @@ const nextConfig = {
         permanent: true,
       },
     ];
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname),
+      '@components': path.resolve(__dirname, 'components'),
+      '@app': path.resolve(__dirname, 'app'),
+      '@hooks': path.resolve(__dirname, 'hooks'),
+      '@lib': path.resolve(__dirname, 'lib'),
+      '@styles': path.resolve(__dirname, 'styles'),
+    };
+    return config;
   },
 };
 

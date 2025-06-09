492f2cad61dc8a3b86e26bab5691136ea695152f
gvangalen <gvangalen@hotmail.com>
2025-06-09 21:35:59 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'frontend',
-      cwd: '/home/ubuntu/trading-tool-frontend',
       script: 'npm',
-      args: 'run start',              // 🔁 Start via npm script (dus `next start`)
+      args: 'start',
+      cwd: '/home/ubuntu/trading-tool-frontend',
       exec_mode: 'fork',
-      interpreter: 'node',            // ✅ Wordt genegeerd bij npm-script maar mag blijven
+      interpreter: 'node',
       watch: false,
       env: {
         NODE_ENV: 'production',

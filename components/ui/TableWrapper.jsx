ed562b2b7bcc8a3b2039fef08f085add3bd13961
gvangalen <gvangalen@hotmail.com>
2025-06-18 21:55:36 +0200

    Delete components/ui/TableWrapper.jsx

--- components/ui/TableWrapper.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function TableWrapper({ children }) {
-  return (
-    <div className="overflow-x-auto rounded-lg shadow border border-gray-200">
-      <table className="w-full text-sm text-left">{children}</table>
-    </div>
-  );
-}

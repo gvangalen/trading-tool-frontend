a7c6a08d952fefee8eeaad770b3ea950303cdd03
gvangalen <gvangalen@hotmail.com>
2025-06-23 20:33:57 +0200

    Update NotificationCard.jsx

--- components/cards/NotificationCard.jsx
+++ components/cards/NotificationCard.jsx
@@ -1,15 +1,17 @@
 'use client';
 
-import CardWrapper from '@/components/ui/CardWrapper';
+import { Card, CardContent } from '@/components/ui/card';
 
 export default function NotificationCard({ icon, title, subtitle, color = 'bg-white' }) {
   return (
-    <CardWrapper className={`${color}`}>
-      <div className="text-xl mb-1 flex items-center gap-2">
-        {icon}
-        <strong>{title}</strong>
-      </div>
-      <p className="text-sm text-gray-700">{subtitle}</p>
-    </CardWrapper>
+    <Card className={`${color} w-full sm:w-60`}>
+      <CardContent>
+        <div className="text-xl mb-1 flex items-center gap-2">
+          {icon}
+          <strong>{title}</strong>
+        </div>
+        <p className="text-sm text-gray-700">{subtitle}</p>
+      </CardContent>
+    </Card>
   );
 }

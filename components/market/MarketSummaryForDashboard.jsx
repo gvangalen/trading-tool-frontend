1ec32a9febb06e5afc60ae76f5904157ec42ab0e
gvangalen <gvangalen@hotmail.com>
2025-09-27 19:54:58 +0200

    Update MarketSummaryForDashboard.jsx

--- components/market/MarketSummaryForDashboard.jsx
+++ components/market/MarketSummaryForDashboard.jsx
@@ -1,13 +1,21 @@
 'use client';
 
-import MarketLiveCard from './MarketLiveCard';
-import MarketSevenDayTable from './MarketSevenDayTable';
+import { useMarketData } from '@/hooks/useMarketData';
+import MarketLiveCard from '@/components/market/MarketLiveCard';
+import MarketSevenDayTable from '@/components/market/MarketSevenDayTable';
 
 export default function MarketSummaryForDashboard() {
+  const { sevenDayData, btcLive } = useMarketData();
+
   return (
     <div className="space-y-4">
-      <MarketLiveCard />
-      <MarketSevenDayTable />
+      <MarketLiveCard
+        price={btcLive?.price}
+        change24h={btcLive?.change_24h}
+        volume={btcLive?.volume}
+        timestamp={btcLive?.timestamp}
+      />
+      <MarketSevenDayTable history={sevenDayData} />
     </div>
   );
 }

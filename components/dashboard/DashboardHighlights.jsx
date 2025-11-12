6cc193dd11226b1e5168eda67831ba610b43ed4e
gvangalen <gvangalen@hotmail.com>
2025-11-12 20:44:37 +0100

    Update DashboardHighlights.jsx

--- components/dashboard/DashboardHighlights.jsx
+++ components/dashboard/DashboardHighlights.jsx
@@ -2,7 +2,7 @@
 
 import ReportCard from '@/components/cards/ReportCard';
 import ActiveTradeCard from '@/components/cards/ActiveTradeCard';
-import TechnicalScoreCard from '@/components/cards/TechnicalScoreCard';
+import MasterScoreCard from '@/components/dashboard/MasterScoreCard';
 import TradingBotCard from '@/components/cards/TradingBotCard';
 
 export default function DashboardHighlights() {
@@ -10,7 +10,7 @@ export default function DashboardHighlights() {
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       <ReportCard />
       <ActiveTradeCard />
-      <TechnicalScoreCard />
+      <MasterScoreCard />
       <TradingBotCard />
     </div>
   );

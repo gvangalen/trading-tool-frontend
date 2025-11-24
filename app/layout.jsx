ad4f31f5b6411d0003a9b6e84c405a1663439beb
gvangalen <gvangalen@hotmail.com>
2025-11-24 19:12:10 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,21 @@
-import '../styles/globals.css';
-import 'rc-slider/assets/index.css';
-
-import NavBar from '@/components/ui/NavBar';
-import TopBar from '@/components/ui/TopBar';
-import { Toaster } from 'react-hot-toast';
+import NavBar from "@/components/ui/NavBar";
+import TopBar from "@/components/ui/TopBar";
+import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="nl">
       <body className="bg-[var(--bg)] text-[var(--text-dark)]">
-
         <Toaster position="top-right" />
 
         <NavBar />
 
-        {/* PAGE SHELL */}
-        <div className="ml-64"> 
+        <div className="ml-64">
           <TopBar />
-
-          <main className="pt-20 px-6 pb-6">
+          <main className="px-6 pt-20 pb-10">
             {children}
           </main>
         </div>
-
       </body>
     </html>
   );

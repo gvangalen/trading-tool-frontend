ef54cbbe7f422cd7c80809f947ec203be79e3ec3
gvangalen <gvangalen@hotmail.com>
2026-01-01 14:06:04 +0100

    Update NarrativeBlock.jsx

--- components/report/blocks/NarrativeBlock.jsx
+++ components/report/blocks/NarrativeBlock.jsx
@@ -1,1 +1,21 @@
+import ReportCard from '../ReportCard';
+import { FileText } from 'lucide-react';
 
+export default function NarrativeBlock({
+  title,
+  text,
+  color = 'gray',
+}) {
+  if (!text) return null;
+
+  return (
+    <ReportCard
+      icon={<FileText size={18} />}
+      title={title}
+      pre
+      color={color}
+    >
+      {text}
+    </ReportCard>
+  );
+}

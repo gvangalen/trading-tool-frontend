ec4cf4292b718f94fff7429cbd351b709113064c
gvangalen <gvangalen@hotmail.com>
2026-02-13 13:39:20 +0100

    Delete app/AuthClientProvider.jsx

--- app/AuthClientProvider.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AuthProvider } from "@/components/auth/AuthProvider";
-
-export default function AuthClientProvider({ children }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}

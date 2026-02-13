b9389160bd1f7b576a358fb883c32018355327a3
gvangalen <gvangalen@hotmail.com>
2026-02-13 13:42:27 +0100

    Update layout.jsx

--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -2,46 +2,36 @@
 
 import NavBar from "@/components/ui/NavBar";
 import TopBar from "@/components/ui/TopBar";
-import { Toaster } from "react-hot-toast";
-
-import ClientProviders from "../ClientProviders";
-import AuthClientProvider from "../AuthClientProvider";
 import AuthGuard from "@/components/auth/AuthGuard";
 
 export default function ProtectedLayout({ children }) {
   return (
-    <AuthClientProvider>
-      <ClientProviders>
-        <AuthGuard>
-          <Toaster position="top-right" />
+    <AuthGuard>
+      {/* Sidebar */}
+      <NavBar />
 
-          {/* Sidebar */}
-          <NavBar />
+      {/* Content wrapper */}
+      <div className="md:pl-64">
+        {/* TopBar */}
+        <div
+          className="
+            fixed top-0
+            left-0 md:left-64 right-0
+            h-16
+            topbar-surface
+            z-40
+            rounded-bl-3xl
+            shadow-lg
+          "
+        >
+          <TopBar />
+        </div>
 
-          {/* Content wrapper */}
-          <div className="md:pl-64">
-            {/* TopBar */}
-            <div
-              className="
-                fixed top-0
-                left-0 md:left-64 right-0
-                h-16
-                topbar-surface
-                z-40
-                rounded-bl-3xl
-                shadow-lg
-              "
-            >
-              <TopBar />
-            </div>
-
-            {/* Page */}
-            <main className="pt-16 px-4 md:px-8 pb-14 min-h-screen">
-              {children}
-            </main>
-          </div>
-        </AuthGuard>
-      </ClientProviders>
-    </AuthClientProvider>
+        {/* Page */}
+        <main className="pt-16 px-4 md:px-8 pb-14 min-h-screen">
+          {children}
+        </main>
+      </div>
+    </AuthGuard>
   );
 }
